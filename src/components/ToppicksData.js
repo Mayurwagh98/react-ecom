6d125110aa53b895ed33e016cca13b9d0bc1c53a
Mayurwagh98 <mswagh1998@gmail.com>
2022-11-30 23:17:22 +0530

    Added:images data

---
+++ src/components/ToppicksData.js
@@ -0,0 +1,48 @@
+let data = [
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpM0jIUOOXt3k-leG-ja7RkFxgIcq7qk-hOA&usqp=CAU",
+        title: "Kurta"
+
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQcR4Gqj72httlqiZAVrxI3e0XtpsdEthUpg&usqp=CAU",
+        title: "Kurta"
+
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Im0ZIGl32-EqfKCvDAaWudV7AA895mqWyw&usqp=CAU",
+        title: "Kurta"
+
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD6pqcgk6x_BMcH_M7vT9DV4jpoIF5c03Okg&usqp=CAU",
+        title: "Kurta"
+
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNfYiKsupH0JGhzIKNL05Gcr09VCvRGTxXHA&usqp=CAU",
+        title: "Kurta"
+    }
+]
+
+
+    let today_deals_images= [
+        {
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjegDB8Ckz85PAHfD8dtHoKLeqz0z3Zjor9g&usqp=CAU",
+            title: "Summer Wears"
+        },
+        {
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaeWksuTqv7vOgRQOETFU9drGW5eRGBPkGHA&usqp=CAU",
+            title: "Winter Wears"
+        },
+        {
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWAjm50U9mXhc1aMqTgrMfjdvpTS8OCJLJ-Q&usqp=CAU",
+            title: "Monsoon Wears"
+        },
+        {
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRblcJngJwfRZ0iVrOXILtGbRKGGkiP-U0kaQ&usqp=CAU",
+            title: "Casual Wears"
+        }
+    ]
+
+export {data, today_deals_images}
