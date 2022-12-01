ec5232a47a6047195b007aa203538ab20f899898
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-01 17:21:10 +0530

    Added: Route for Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react";
+
 import { Navbar } from "./components/Navbar";
 import "./App.css";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Product } from "./components/Product";
 import {Routes, Route} from "react-router-dom"
+import { Footer } from "./components/Footer";
+import {Error} from "./components/Error"
+import { Login } from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />}  />
         <Route exact path="/product" element={<Product />}  />
+        <Route exact path="/login" element={<Login />}  />
+        <Route path="*" element={<Error />}/>
       </Routes>
+
+      <Footer />
     </div>
   );
 }
