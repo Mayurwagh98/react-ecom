854bdc666d025736bc9bdc9378708deae6a6e7dc
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-01 17:19:23 +0530

    Updated: Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,38 @@
 import React from "react";
 import { SlideShow } from "./SlideShow";
+import { TodayDelas } from "./TodayDeals";
+import { Toppicks } from "./Toppicks";
+import { data, today_deals_images } from "./ToppicksData";
 
 let Home = () => {
   return (
     <>
       {/* <h1>Home</h1> */}
       {/* <Link to="/" ><Home /></Link> */}
+
       <div className="slide_show">
         <SlideShow />
       </div>
       <br />
-      <h1>Today's Deals</h1>
+      <h1>Deal of the Day</h1>
       <div className="today_deals">
-        <div>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyUVWJ645_SI5LWf_okXWIKKuokeq2MvOF1A&usqp=CAU"
-            alt=""
-          />
-          <br />
-          Summer Wears
-        </div>
-        <div>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJQW9naFiEn04gZabBDDXwzBywZO_erYIb3A&usqp=CAU"
-            alt=""
-          />
-          <br />
-          Winter Wear
-        </div>
-        <div>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYJ00qWQLEZWw3p3M21RqguSRfMRlqfiB2_w&usqp=CAU"
-            alt=""
-          />
-          <br />
-          Monsoon Wear
-        </div>
-        <div>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8UOLTOFrKSuTGrCPCYE6cXDmUzJVV_lN9XQ&usqp=CAU"
-            />
-          <br />
-          Casual Wear
-        </div>
+        {today_deals_images.map((ele, index) => {
+          return (
+            <div key={index}>
+              <TodayDelas imgsrc={ele.img} title={ele.title}  />
+            </div>
+          );
+        })}
+      </div>
+      <h1>Top Picks</h1>
+      <div className="top_picks">
+        {data.map((ele, index) => {
+          return (
+            <div key={index}>
+              <Toppicks imgsrc={ele.img}  title={ele.title} />
+            </div>
+          );
+        })}
       </div>
     </>
   );
