14ec841892c2329047eafa8068a9601ddbf6df4b
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-02 19:04:27 +0530

    Added: Hardcoded Product Page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { products } from "./ToppicksData";
 
-let Product = () =>{
+let Product = () => {
+  return (
+    <>
+      <h1>Product</h1>
+      <div className="products_div">
+        {products.map((ele, index) => {
+          return (
+            <div key={index}>
+              <img src={ele.img} alt="products_image" />
+              <div className="price_div">
+                <h2>{ele.title}</h2>
+                <h4>{`Price:- ${ele.price}`}</h4>
+              </div>
+              <button style={{margin: "5px"}}>Add to Cart</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-    return(
-
-        <>
-        <h1>Product</h1>
-        </>
-    )
-}
-
-export {Product}
+export { Product };
