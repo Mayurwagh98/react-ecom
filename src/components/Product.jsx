586a7b54554420de1858e51f6f11302dc8c50d05
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-05 03:11:42 +0530

    Added: Fetched Products from API

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import { products } from "./ToppicksData";
+import React, { useState } from "react";
+// import { products } from "./ToppicksData";
+import axios from "axios";
+import { useEffect } from "react";
 
 let Product = () => {
+  let [data, setData] = useState([]);
+
+  let getData = async () => {
+    await axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        // let data = res.data;
+        // console.log(data);
+        data = res.data;
+        setData(data);
+        console.log(data);
+        // console.log(res.data)
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    // setData(res.data)
+    // console.log(res)
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <h1>Product</h1>
-      <div className="products_div">
+      {/* <div className="products_div">
         {products.map((ele, index) => {
           return (
             <div key={index}>
@@ -14,8 +40,23 @@ let Product = () => {
                 <h2>{ele.title}</h2>
                 <h4>{`Price:- ${ele.price}`}</h4>
               </div>
-              <button style={{margin: "5px"}}>Add to Cart</button>
+              <button style={{ margin: "5px" }}>Add to Cart</button>
             </div>
+          );
+        })}
+      </div> */}
+      <div className="products_div">
+        {data.map((item, index) => {
+          return (
+            
+              <div key={index}>
+                <img src={item.image} alt="images" />
+                <div className="price_div">
+                  <h2>{item.price}</h2>
+                  <h4>{item.title}</h4>
+                </div>
+              </div>
+           
           );
         })}
       </div>
