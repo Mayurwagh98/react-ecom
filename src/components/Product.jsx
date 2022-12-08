6f3fad17839aec4b39388ede70fe807d6efbe004
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-08 08:59:51 +0530

    Added: Tooltip Icon

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,14 +6,12 @@ import { useEffect } from "react";
 // import RedoIcon from "@mui/icons-material/Redo";
 // import KeyboardTabIcon from '@mui/icons-material/KeyboardTab';
 // import UndoIcon from "@mui/icons-material/Undo";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Tooltip from "@mui/material/Tooltip";
 
 let Product = () => {
   let [data, setData] = useState([]); //getting whole data
   let [page, setPage] = useState(1);
-  // let [pageData, setpageData] = useState(0); //hook to from where pageData is starting
-  // let [displayData, setdisplayData] = useState([]); //data which we want to dislay after splitting it
-  // let perpageData = 5;
-  // let offset = page * perpageData 
   // https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products?limit=10&page=1
   let getData = async () => {
     await axios
@@ -22,14 +20,10 @@ let Product = () => {
         `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products?limit=10&page=${page}`
       )
       .then((res) => {
-        // let data = res.data;
-        // console.log(data);
-        // res = res.json()
         data = res.data.data;
-        // data = res.data;
+
         setData(data);
-        // let sliceData = data.slice(offset, perpageData+offset);
-        // setdisplayData(sliceData)
+
         console.log(data);
 
         // console.log(res.data)
@@ -37,21 +31,11 @@ let Product = () => {
       .catch((e) => {
         console.log(e);
       });
-    // setData(res.data)
-    // console.log(res)
   };
-
-  // useEffect(() => {
-  //   let initialloadData = data.split(0, perpageData);
-  //   setdisplayData(initialloadData);
-  //   console.log(displayData)
-  // }, []);
 
   useEffect(() => {
     getData();
   }, [page]);
-
-
 
   let addtoCart = (item) => {
     // console.log(item)
@@ -60,7 +44,7 @@ let Product = () => {
     localData.push(item);
 
     localStorage.setItem("CartItem", JSON.stringify(localData));
-
+    alert(`Product added to the cart`);
     console.log(localData);
   };
 
@@ -70,15 +54,8 @@ let Product = () => {
       setPage((page += 1));
     }
 
-    // let sliceData = data.slice(offset, perpageData+offset);
-    // setdisplayData(sliceData);
-    // setPage((page += 1));
-    // setData(data)
-    // getData()
-    
     console.log(data);
   };
-
 
   let handlePrev = () => {
     if (page == 1) {
@@ -91,8 +68,6 @@ let Product = () => {
     // getData()
   };
 
-  // let pageCount = Math.ceil(data.length / perpageData)
-  
   return (
     <>
       <h1>Product</h1>
@@ -133,32 +108,18 @@ let Product = () => {
                 <h2>{item.price}</h2>
                 <h4>{item.title}</h4>
               </div>
-              <button onClick={() => addtoCart(item)} style={{ margin: "5px" }}>
-                Add to Cart
-              </button>
+              <Tooltip title="Add to Cart">
+                <AddShoppingCartIcon
+                  onClick={() => addtoCart(item)}
+                  style={{ margin: "5px" }}
+                >
+                  Add to Cart
+                </AddShoppingCartIcon>
+              </Tooltip>
             </div>
           );
         })}
       </div>
-
-
-      {/* <div className="products_div">
-        
-        {displayData.map((item, index) => {
-          return (
-            <div key={index}>
-              <img src={item.image} alt="images" />
-              <div className="price_div">
-                <h2>{item.price}</h2>
-                <h4>{item.title}</h4>
-              </div>
-              <button onClick={() => addtoCart(item)} style={{ margin: "5px" }}>
-                Add to Cart
-              </button>
-            </div>
-          );
-        })}
-      </div> */}
     </>
   );
 };
