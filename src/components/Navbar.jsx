b7b71f2deb2c62bbd4e46d008574978b886b7496
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-08 08:59:16 +0530

    Added: Tooltip and Other Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,62 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+// import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import LoginIcon from "@mui/icons-material/Login";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import InfoIcon from "@mui/icons-material/Info";
+import HomeIcon from "@mui/icons-material/Home";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Tooltip from "@mui/material/Tooltip";
 
 let Navbar = () => {
   return (
     <>
-      <div className="navbar">
-      <Link to="/" className="home_link">Home</Link>
-      <Link to="/about" className="about_link">About</Link>
-      <Link to="/product" className="product_link">Product</Link>
+      <div className="navbar conMain">
+        <Link to="/" className="home_link">
+          {/* Home Link*/}
+          <Tooltip title="Home">
+            <HomeIcon />
+          </Tooltip>
+        </Link>
+
+        <Link to="/about" className="home_link">
+          {/* About Link*/}
+          <Tooltip title="About">
+            <InfoIcon />
+          </Tooltip>
+        </Link>
+
+        <Link to="/product" className="home_link">
+          {/* Products Link*/}
+          <Tooltip title="Products">
+            <InventoryIcon />
+          </Tooltip>
+        </Link>
+
+        <Link to="/login" className="home_link">
+          {/* Login Link*/}
+          <Tooltip title="Login">
+            <LoginIcon />
+          </Tooltip>
+        </Link>
+
+        <Link to="/signup" className="home_link">
+          {/* Signup Link*/}
+          <Tooltip title="Signup">
+            <SubscriptionsIcon />
+          </Tooltip>
+        </Link>
+
+        <Link to="/cart" className="home_link">
+          {/* Cart Link*/}
+          <Tooltip title="Cart">
+            <ShoppingCartIcon />
+          </Tooltip>
+        </Link>
       </div>
     </>
   );
 };
 
-
-export {Navbar}
+export { Navbar };
