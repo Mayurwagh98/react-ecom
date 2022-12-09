31206b4993d004664af1828c4d7ff466a86608b1
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-09 07:16:47 +0530

    Added: Total Functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,29 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Tooltip from "@mui/material/Tooltip";
+import { linkClasses } from "@mui/material";
+import { useEffect } from "react";
 
 let Cart = () => {
-  let [data, setData] = useState([]);
-
-  let [count, setCount] = useState(0);
-
   let localData = JSON.parse(localStorage.getItem("CartItem")) || [];
+  let [displayCart, setDisplayCart] = useState([...localData]);
 
   let deleteItem = (i) => {
     localData.splice(i, 1);
 
-    // localData.push(item)
-
     localStorage.setItem("CartItem", JSON.stringify(localData));
 
-    data = localData;
-
-    setData(data);
+    setDisplayCart([...localData]);
   };
+
+  let handleQty = (inre_decre, item) => {
+    console.log(item);
+
+    if (inre_decre === "Add") {
+      item.qty++;
+    } else if (inre_decre === "Sub") {
+      item.qty--;
+    }
+
+    console.log(item);
+    console.log(localData);
+    localStorage.setItem("CartItem", JSON.stringify([...localData]));
+    setDisplayCart([...localData]);
+  };
+
+  let sum = 0;
+  for (let x of localData) {
+    sum += x.price * x.qty;
+  }
+  // console.log(total);
 
   return (
     <>
-      <h1 style={{ marginTop: "100px" }}>Cart:-{(count = localData.length)}</h1>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "80px",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <h1>Cart:-{localData.length}</h1>
+        <h1>Total:- {sum}</h1>
+      </div>
       <div className="products_div">
         {localData.map((item, index) => {
           return (
@@ -33,8 +58,30 @@ let Cart = () => {
                 <h2>{item.price}</h2>
                 <h4>{item.title}</h4>
               </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  background: "transparent",
+                  margin: "auto",
+                  padding:"5px"
+                }}
+              >
+                <button onClick={() => handleQty("Add", item)}>+</button>
+                <h3>{item.qty}</h3>
+                <button
+                  onClick={() => handleQty("Sub", item)}
+                  disabled={item.qty === 1}
+                >
+                  -
+                </button>
+              </div>
+
               <Tooltip title="Delete">
-                <DeleteIcon onClick={() => deleteItem(item)}>Delete</DeleteIcon>
+                <DeleteIcon onClick={() => deleteItem(index)} style={{paddig:"5px", margin:"5px"}}>
+                  Delete
+                </DeleteIcon>
               </Tooltip>
             </div>
           );
