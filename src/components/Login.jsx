547a2b58090c8217cd4cc735a614130c27d35bd7
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-10 06:26:10 +0530

    Added: Login Logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useHistory } from "react";
+import { Navigate, redirect } from "react-router-dom";
 
 let Login = () => {
-  let [formData, setformData] = useState({
-    fname: "",
-    email: "",
-    pass: "",
+  let [loginformData, setloginformData] = useState({
+    loginFname: "",
+    loginEmail: "",
+    loginPass: "",
   });
+
+  let signupData = JSON.parse(localStorage.getItem("Formdata")) 
+  let loginDetails = JSON.parse(localStorage.getItem("LoginDetails")) || [];
+  // console.log(signupData)
+  
+  let handleLoginform = (event) => {
+    let { name, value } = event.target;
+    setloginformData({
+      ...loginformData,
+      [name]: value,
+    });
+  };
+
+  let loginFormSubmit = (event) => {
+    event.preventDefault();
+    
+    let flag = false;
+    for (let x of signupData) {
+      if (x.email === loginformData.loginEmail) {
+        flag = true;
+      }
+    }
+    if (flag) {
+      loginDetails.push(loginformData);
+      localStorage.setItem("LoginDetails", JSON.stringify(loginDetails));
+      // console.log(loginDetails);
+      setloginformData({ loginFname: "", loginEmail: "", loginPass: "" });
+      alert(`Login Successfull`);
+    } else {
+      alert(`Register first`);
+    }
+
+    // setloginformData(<Navigate to="/product"/>)
+  };
 
   return (
     <>
       <h1 style={{ marginTop: "200px" }}>Login page</h1>
       <div>
-        <input
-          type="text"
-          placeholder="enter your name"
-          value={formData.fname}
-          onChange={(e) => setformData(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="enter your email"
-          value={formData.email}
-          onChange={(e) => setformData(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="enter password"
-          value={formData.pass}
-          onChange={(e) => setformData(e.target.value)}
-        />
+        <form onSubmit={loginFormSubmit}>
+          <input
+            type="text"
+            placeholder="enter your name"
+            name="loginFname"
+            value={loginformData.loginFname}
+            onChange={handleLoginform}
+          />
+          <input
+            type="text"
+            placeholder="enter your email"
+            name="loginEmail"
+            value={loginformData.loginEmail}
+            onChange={handleLoginform}
+          />
+          <input
+            type="password"
+            placeholder="enter password"
+            name="loginPass"
+            value={loginformData.loginPass}
+            onChange={handleLoginform}
+          />
+          <input type="submit" />
+        </form>
       </div>
     </>
   );
