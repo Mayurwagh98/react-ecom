9ac2b970da1ac83dc2da3dc66c6ee61fb445ef26
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-14 15:23:59 +0530

    Made: Few Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-let Footer = () =>{
-    
-    let year = new Date().getFullYear()
+let Footer = () => {
+  let year = new Date().getFullYear();
 
-    return(
+  return (
+    <>
+      <h4>{year} All rights reserved</h4>
+      <h4>
+        <hr />
+      </h4>
+    </>
+  );
+};
 
-        <>
-        <h4>{year} All right reserved</h4>
-        <h4>
-            <hr />
-        </h4>
-
-        </>
-    )
-}
-
-export {Footer}
+export { Footer };
